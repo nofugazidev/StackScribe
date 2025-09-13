f2cfdc1aaa137bd2eaa6a39ee225773aa24ad53d
nofugazidev <oyebisimubarak4@gmail.com>
2025-09-13 22:45:04 +0100

    wip:home/dashboard UI implementation

--- src/components/dashboardLayout.tsx
+++ src/components/dashboardLayout.tsx
@@ -27,6 +27,8 @@ const DashboardLayout: React.FC = () => {
   const auth = useAuth();
 
   const user = auth?.user;
+  const logout = auth?.logout;
+  
   return (
     <>
       <main className="flex items-start">
@@ -38,29 +40,53 @@ const DashboardLayout: React.FC = () => {
           </header>
 
           <section className="dash-menu pt-10">
-            <p onClick={() => setActivePage("home")} className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${activePage === "home" ? "font-medium" : "font-normal"} `}>
+            <p
+              onClick={() => setActivePage("home")}
+              className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${
+                activePage === "home" ? "font-medium" : "font-normal"
+              } `}
+            >
               <BiHome className="" />
               home
             </p>
-            <p onClick={() => setActivePage("createBlog")} className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${activePage === "createBlog" ? "font-medium" : "font-normal"} `}>
+            <p
+              onClick={() => setActivePage("createBlog")}
+              className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${
+                activePage === "createBlog" ? "font-medium" : "font-normal"
+              } `}
+            >
               <BiBookAdd className="" />
               create Blog
             </p>
-            <p onClick={() => setActivePage("editBlog")} className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${activePage === "editBlog" ? "font-medium" : "font-normal"} `}>
+            <p
+              onClick={() => setActivePage("editBlog")}
+              className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${
+                activePage === "editBlog" ? "font-medium" : "font-normal"
+              } `}
+            >
               <BiPencil className="" />
               edit Blog
             </p>
-            <p onClick={() => setActivePage("settings")} className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${activePage === "settings" ? "font-medium" : "font-normal"} `}>
+            <p
+              onClick={() => setActivePage("settings")}
+              className={`flex items-center gap-2 text-[20px] capitalize cursor-pointer pt-7 ${
+                activePage === "settings" ? "font-medium" : "font-normal"
+              } `}
+            >
               <BiCog className="" />
               settings
             </p>
           </section>
         </div>
         <div className="right flex-[.85]">
-          <div className="top">
-            <h1>{user?.email}</h1>
+          <div className="top border-b-2 px-4 py-5 flex flex-row items-center justify-between">
+            <h3 className="font-medium text-2xl capitalize">welcome back 👋</h3>
+            <div className="flex flex-row gap-4 items-center">
+              <h1 className="lowercase font-semibold underline">{user?.email}</h1>
+              <button onClick={logout} disabled={!logout} className="rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 px-5 capitalize text-white font-medium cursor-pointer">logout</button>
+            </div>
           </div>
-          <div className="main-area">{renderPage()}</div>
+          <div className="main-area px-4 py-5">{renderPage()}</div>
         </div>
       </main>
     </>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+      <section>
+        <header>
+          <div className="top">
+            <input
+              className="title text-3xl font-bold outline-none cursor-text pb-3 text-gray-500 w-full"
+              placeholder="Write Title Here..."
+            />
+            <input
+              type="text"
+              className="subtext text-lg font-normal outline-none cursor-text pb-3 text-gray-500 w-full"
+              placeholder="Enter subtext here..."
+            />
+            <div className="toolbar border border-1 h-20 w-full rounded bg-black/60"></div>
+          </div>
+        </header>
+        <main className="mt-10">
+          <textarea
+            name=""
+            id=""
+            rows={2}
+            className="w-full border-2 rounded outline-none p-5"
+            placeholder="Write here..."
+          ></textarea>
+          <button className="rounded bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 px-5 capitalize text-white font-medium cursor-pointer w-full mt-5">
+            post
+          </button>
+        </main>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
